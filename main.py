b68f21fd20ba8ff0f1f550ecd9384552d9f05339
Lugina <lugina.oezdemir@code.berlin>
2022-11-30 17:50:49 +0100

    adding range

--- main.py
+++ main.py
@@ -3,17 +3,6 @@ import numpy as np
 grid = [[0,1,0], [0,1,0],[0,1,0]]
 print(grid)
 
-#print(grid[2][1])
-
-print(iter(grid))
-           
-for i in map(iter, grid[0]):
-    if i < i:
-        i==0
-    elif i > i+1:
-        i+=1
-    elif i == i:
-        next(i)
- 
-
-    
+for i in range(3):
+    for j in range(3):
+        
