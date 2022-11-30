25341ed3e98d42a6cb5b5381ca13cbca49abfbea
Lugina <lugina.oezdemir@code.berlin>
2022-11-30 16:55:52 +0100

    created for loop

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
 import numpy as np
 
-grid = np.array([[[0,1,0],[0,1,0],[0,1,0]]])
+grid = [[1,11,111],[2,22,222],[3,33,333]]
 print(grid)
+
+#print(grid[2][1])
+
+print(iter(grid))
+           
+for i in iter(grid):
+    for j in iter(i):
+        for k in iter(j):
+            if i < i+1:
+                i=0
+            elif i > i+1:
+                i+1
+            elif i == i:
+                next(i)
+    
