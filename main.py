36af757e2ccef8eff6153e3e6eb006d8d5ba978f
Lugina <lugina.oezdemir@code.berlin>
2022-11-30 17:06:34 +0100

    corrected grid values

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import numpy as np
 
-grid = [[1,11,111],[2,22,222],[3,33,333]]
+grid = [[0,1,0], [0,1,0],[0,1,0]]
 print(grid)
 
 #print(grid[2][1])
 
 print(iter(grid))
            
-for i in iter(grid):
-    for j in iter(i):
-        for k in iter(j):
-            if i < i+1:
-                i=0
-            elif i > i+1:
-                i+1
-            elif i == i:
-                next(i)
+for i in map(iter, grid[0]):
+    if i < i:
+        i==0
+    elif i > i+1:
+        i+=1
+    elif i == i:
+        next(i)
+ 
+
     
